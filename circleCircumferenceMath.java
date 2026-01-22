0d45a6e3c244bba8b7d5da141c1826fe4f6523e8
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-22 23:52:01 +0530

    circumference Java

--- circleCircumferenceMath.java
+++ circleCircumferenceMath.java
@@ -8,7 +8,7 @@ public class circleCircumferenceMath {
         radius = scanner.nextDouble();
         scanner.nextLine();
 
-        double circumference1 = 2 * Math.PI * radius;
+        double circumference = 2 * Math.PI * radius;
         double area = Math.PI * Math.pow(radius , 2);
         double volume = (4.0/3.0) * Math.PI * Math.pow(radius , 3);
 
@@ -17,7 +17,7 @@ public class circleCircumferenceMath {
         ch = scanner.nextLine();
 
         if(ch.equals("C")){
-            System.out.printf("The circumference is : %.2fcm" , circumference1);
+            System.out.printf("The circumference is : %.2fcm" , circumference);
         }
         else if(ch.equals("A")){
             System.out.printf("The Area is : %.1fcm" , area);
