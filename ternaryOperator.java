03aa8ce777eb06d5f2989d5c6f5c4c70b06f5449
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-22 12:41:24 +0530

    TernaryOperator

---
+++ ternaryOperator.java
@@ -0,0 +1,25 @@
+public class ternaryOperator {
+    public static void main(String[] args){
+
+        // int score = 70;
+
+        // if(score >= 60){
+        //     System.out.println("PASS");
+        // }
+        // else{
+        //     System.out.println("FAIL");
+        // }
+
+        // String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+
+        // System.out.println(passOrFail);
+
+
+        int number = 6;
+       
+        String evenOrOdd = (number%2 == 0) ? "Even" : "Odd";
+
+        System.out.println(evenOrOdd);
+
+    }
+}
