805c9d6391e13c103dbb3ff783172bf254fab437
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-24 16:26:05 +0530

    NestedLoopInJava

---
+++ NestedLoop.java
@@ -0,0 +1,13 @@
+public class NestedLoop {
+    public static void main(String[] args){
+
+//        nested loop = A loop inside other loop
+
+        for(int i = 0 ; i <= 3 ; i++){
+            for(int j = 0 ; j <= 6 ; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
