8a5d4277a282ec280b20545c33b1ee4a1c06e548
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-26 11:53:23 +0530

    OOPSInJava

---
+++ oops/Constructor.java
@@ -0,0 +1,24 @@
+package oops;
+
+public class Constructor {
+    public static void main(String[] args){
+
+//        constructor = A special method to intialize objects
+
+        Student student1 = new Student("SpongeBob" , 30 , 3.2);
+        Student student2 = new Student("Patrick" , 34 , 1.5);
+
+//        System.out.println(student1.name);
+//        System.out.println(student2.name);
+//        System.out.println(student1.age);
+//        System.out.println(student2.age);
+//        System.out.println(student1.gpa);
+//        System.out.println(student2.gpa);
+//        System.out.println(student1.isEnrolled);
+//        System.out.println(student2.isEnrolled);
+
+        student1.study();
+        student2.study();
+
+    }
+}
---
+++ oops/OverLoadedConstructors.java
@@ -0,0 +1,33 @@
+package oops;
+
+public class OverLoadedConstructors {
+    public static void main(String[] args){
+
+//        overloaded constructors = Allow a class to have multiple                constructors with different parameter lists.
+
+
+         User user1 = new User("SpongeBob");
+         User user2 = new User("Patrick" , "PStar@aol.com");
+         User user3 = new User("Kartik" , "Kartikverma1074@gmail.com" , 19);
+         User user4 = new User();
+
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+
+
+    }
+}
