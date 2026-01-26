bc3f36d738b995f3bb918376994332c85d023951
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-26 11:17:44 +0530

    OOPSInJava

---
+++ oops/ObjectOrientedProgramming.java
@@ -0,0 +1,31 @@
+package oops;
+
+public class ObjectOrientedProgramming {
+    public static void main(String[] args){
+
+
+//        Object = It is an entity that cna hold Data
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+//        car.isRunning = true;
+//
+//        System.out.println(car.model);
+//        System.out.println(car.make);
+//        System.out.println(car.year);
+//        System.out.println(car.price);
+//        System.out.println(car.isRunning);
+
+//        System.out.println(car.isRunning);
+//        car.start();
+//        System.out.println(car.isRunning);
+//        car.stop();
+//        System.out.println(car.isRunning);
+
+//        car.drive();
+//        car.brake();
+
+
+    }
+}
