ffe90264ae22c79d9dcab948301d1ff82ce3930e
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-26 13:06:48 +0530

    OOPSInJava

---
+++ oops/StaticKeyWord.java
@@ -0,0 +1,18 @@
+package oops;
+
+public class StaticKeyWord {
+    public static void main(String[] args){
+
+//      static = Makes a variable or method belong to the class rather than to any specific object.
+
+        Friend friend1 = new Friend("Kartik");
+        Friend friend2 = new Friend("Paras");
+        Friend friend3 = new Friend("Shalok");
+        Friend friend4 = new Friend("Agrim");
+
+//        System.out.println(friend1.numberOfFriends);
+//        System.out.println(friend2.numberOfFriends);
+
+        Friend.showFriends();
+    }
+}
