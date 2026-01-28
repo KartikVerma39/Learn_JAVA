616324e8a14c3cdfadfd9dc25cb62ed17b76169c
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-28 11:01:22 +0530

    OOPSInJava

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Rectangle extends Shape{
+
+    double length;
+    double breadth;
+
+    Rectangle(double length , double breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    @Override
+    double area(){
+        return length * breadth;
+    }
+
+}
