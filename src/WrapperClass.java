fd46f85074506188eb039215459fd8b6a256dd7d
KartikVerma39 <kartikverma1074@gmail.com>
2026-01-29 12:01:42 +0530

    OOPSInJava

---
+++ src/WrapperClass.java
@@ -0,0 +1,37 @@
+package src;
+
+public class WrapperClass {
+    public static void main(String[] args){
+//        Wrapper classes = Allow primitive values (int , char , double ,boolean)
+//                    to be used as objects. "Wrap then in an object"
+//                    Generally, don't wrap primitives unless you need an object
+//                    Allows use of Collections Framework and static Utility Methods
+
+//         Not Preffered
+//        Integer a = new Integer(123);
+//        Double b = new Double(3.14);
+//        Character c = new Character('$');
+//        Boolean d = new Boolean(true);
+
+
+//        AutoBoxing Preffered
+//        Integer a = 123;
+//        Double b = 3.14;
+//        Character c = '$';
+//        Boolean d = true;
+//
+////        UnBoxing
+//         int x = a;
+//         double y = b;
+
+
+//         String a = Integer.toString(123);
+//         String b = Double.toString(3.14);
+//         String c = Character.toString('@');
+//
+//        String x = a + b + c;
+//        System.out.println(x);
+
+
+    }
+}
